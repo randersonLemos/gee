54553e21e56fe450fe9d747ac1a765aa415ff094
Randerson Lemos <randerson.lemos@gmail.com>
2024-03-15 11:24:28 -0300

    Adding ee in javascript

--- js/hello-world.js
+++ js/hello-world.js
@@ -1,1 +1,18 @@
-console.log('Hello world')
+var ee = require('@google/earthengine');
+
+
+// Initialize client library and run analysis.
+var initialize = function() {
+    ee.initialize(null, null, function() {
+      // ... run analysis ...
+    }, function(e) {
+      console.error('Initialization error: ' + e);
+    });
+  };
+  
+  // Authenticate using an OAuth pop-up.
+  ee.data.authenticateViaOauth(YOUR_CLIENT_ID, initialize, function(e) {
+    console.error('Authentication error: ' + e);
+  }, null, function() {
+    ee.data.authenticateViaPopup(initialize);
+  });
